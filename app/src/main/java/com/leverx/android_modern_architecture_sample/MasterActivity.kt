6dce5e82135cea997fbf65bc862035198231cf29
Yahor Aliakseyeu <yahor.aliakseyeu@leverx.com>
2022-05-24 12:27:02 +0300

    add masterScreen

---
+++ app/src/main/java/com/leverx/android_modern_architecture_sample/MasterActivity.kt
@@ -0,0 +1,56 @@
+package com.leverx.android_modern_architecture_sample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leverx.android_modern_architecture_sample.databinding.ActivityMainBinding
+import com.leverx.android_modern_architecture_sample.databinding.ActivityMasterBinding
+import com.leverx.android_modern_architecture_sample.ui.compose.MainComposeActivity
+import com.leverx.android_modern_architecture_sample.ui.main.view.MainActivity
+
+class MasterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMasterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMasterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.pers.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.word.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.auth.setOnClickListener {
+            val intent = Intent(this, MainComposeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mainview.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.game.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.card.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.test.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.bigtest.setOnClickListener {
+//            val intent = Intent(this, MainComposeActivity::class.java)
+//            startActivity(intent)
+        }
+
+    }
+}
